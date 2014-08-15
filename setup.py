cc9d1d8afebc83c00dd8ac1e9a67c7ed065ef043
cordis <cordis@game-mafia.ru>
2014-08-15 16:29:53 +0800

    f version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PySchema',
-    version='0.10.3',
+    version='0.20.3',
     packages=find_packages(),
     author='CordiS',
     author_email='cordis@game-mafia.ru',
